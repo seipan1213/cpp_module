54219e9f14e6993d534bfb7c8ca44f7bbcfa2b3e
seipan1213 <seiju1213@gmail.com>
2022-07-18 12:41:35 +0900

    PhoneBook 完成

--- cpp00/ex01/Contact.cpp
+++ cpp00/ex01/Contact.cpp
@@ -1,5 +1,17 @@
 #include "Contact.hpp"
 
+bool is_num(std::string str)
+{
+	for (int i = 0; i < (int)str.length(); i++)
+	{
+		if (!(str[i] >= '0' && str[i] <= '9'))
+		{
+			return (false);
+		}
+	}
+	return (str.length());
+}
+
 Contact::Contact()
 {
 }
@@ -17,6 +29,7 @@ void Contact::set_contact()
 		std::cout << "Invalid value\n\nfirst name: ";
 		std::getline(std::cin, this->first_name);
 	}
+
 	std::cout << "last name: ";
 	std::getline(std::cin, this->last_name);
 	while (!std::cin.eof() && this->last_name.empty())
@@ -24,6 +37,7 @@ void Contact::set_contact()
 		std::cout << "Invalid value\n\nlast name: ";
 		std::getline(std::cin, this->last_name);
 	}
+
 	std::cout << "nick name: ";
 	std::getline(std::cin, this->nick_name);
 	while (!std::cin.eof() && this->nick_name.empty())
@@ -31,13 +45,15 @@ void Contact::set_contact()
 		std::cout << "Invalid value\n\nnick name: ";
 		std::getline(std::cin, this->nick_name);
 	}
+
 	std::cout << "phone number: ";
 	std::getline(std::cin, this->phone_number);
-	while (!std::cin.eof() && this->phone_number.empty())
+	while (!std::cin.eof() && !is_num(this->phone_number))
 	{
 		std::cout << "Invalid value\n\nphone number: ";
 		std::getline(std::cin, this->phone_number);
 	}
+
 	std::cout << "darkest secret: ";
 	std::getline(std::cin, this->darkest_secret);
 	while (!std::cin.eof() && this->darkest_secret.empty())
@@ -51,17 +67,17 @@ void Contact::show_contact()
 {
 	std::cout << "|" << std::setfill(' ') << std::setw(10);
 	if (this->first_name.length() > 10)
-		std::cout << this->first_name.substr(0, 9) << ".";
-	else
-		std::cout << this->last_name;
-	std::cout << "|" << std::setfill(' ') << std::setw(10);
-	if (this->last_name.length() > 10)
-		std::cout << this->last_name.substr(0, 9) << ".";
+		std::cout << this->first_name.substr(0, 9) + ".";
 	else
 		std::cout << this->first_name;
 	std::cout << "|" << std::setfill(' ') << std::setw(10);
+	if (this->last_name.length() > 10)
+		std::cout << this->last_name.substr(0, 9) + ".";
+	else
+		std::cout << this->last_name;
+	std::cout << "|" << std::setfill(' ') << std::setw(10);
 	if (this->nick_name.length() > 10)
-		std::cout << this->nick_name.substr(0, 9) << ".";
+		std::cout << this->nick_name.substr(0, 9) + ".";
 	else
 		std::cout << this->nick_name;
 	std::cout << "|" << std::endl;
