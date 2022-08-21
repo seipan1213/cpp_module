f45b5cd8b3911afaea15a6cb40972df6cfb2685d
seipan1213 <seiju1213@gmail.com>
2022-08-21 19:34:33 +0900

    05 00 テスト追加

--- cpp05/ex00/main.cpp
+++ cpp05/ex00/main.cpp
@@ -19,7 +19,31 @@ int main()
 	{
 		try
 		{
+			Bureaucrat hoge("++hoge1", 1);
+			std::cout << "Name: " << hoge.getName() << " Grade: " << hoge.getGrade() << std::endl;
+			++hoge;
+		}
+		catch (std::exception &e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+	}
+	{
+		try
+		{
 			Bureaucrat hoge("hoge150--", 150);
+			std::cout << "Name: " << hoge.getName() << " Grade: " << hoge.getGrade() << std::endl;
+			hoge--;
+		}
+		catch (std::exception &e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+	}
+	{
+		try
+		{
+			Bureaucrat hoge("--hoge150", 150);
 			std::cout << "Name: " << hoge.getName() << " Grade: " << hoge.getGrade() << std::endl;
 			hoge--;
 		}
