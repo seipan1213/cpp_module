d1a7bfe5c8419bbc285b73193e85e4168c3f3a7d
seipan1213 <seiju1213@gmail.com>
2022-08-21 12:10:59 +0900

    04 03 テスト修正

--- cpp04/ex03/main.cpp
+++ cpp04/ex03/main.cpp
@@ -2,6 +2,7 @@
 #include "Ice.hpp"
 #include "MateriaSource.hpp"
 #include "Character.hpp"
+#include <iostream>
 
 int main()
 {
@@ -24,7 +25,49 @@ int main()
 		delete bob;
 		delete me;
 		delete src;
+		std::cout << std::endl;
+	}
+	{
+		IMateriaSource *src = new MateriaSource();
+		src->learnMateria(new Ice());
+		src->learnMateria(new Cure());
+		src->learnMateria(new Ice());
+		src->learnMateria(new Cure());
 
-		return 0;
+		ICharacter *me = new Character("me");
+		ICharacter *you = new Character("you");
+		AMateria *tmp;
+		tmp = src->createMateria("ice");
+		me->equip(tmp);
+		tmp = src->createMateria("ice");
+		me->equip(tmp);
+		tmp = src->createMateria("ice");
+		me->equip(tmp);
+		tmp = src->createMateria("ice");
+		me->equip(tmp);
+		tmp = src->createMateria("cure");
+		me->equip(tmp);
+
+		you->equip(tmp);
+		tmp = src->createMateria("cure");
+		you->equip(tmp);
+
+		ICharacter *hoge = new Character("hoge");
+		me->use(0, *hoge);
+		me->use(1, *hoge);
+		me->use(2, *hoge);
+		me->use(3, *hoge);
+		me->use(4, *hoge);
+
+		you->use(0, *me);
+		you->use(1, *me);
+		you->use(2, *me);
+		you->use(3, *me);
+		you->use(4, *me);
+
+		delete hoge;
+		delete me;
+		delete you;
+		delete src;
 	}
 }
