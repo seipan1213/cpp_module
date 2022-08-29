37450dc298013e86430e008396758e0ebf2b729d
seipan1213 <seiju1213@gmail.com>
2022-08-29 20:54:30 +0900

    テスト修正

--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -7,10 +7,7 @@ int main(void)
 	uintptr_t serialized = serialize(&data);
 	Data *deserialized = deserialize(serialized);
 
-	std::cout << "data.str: " << data.str << std::endl;
 	std::cout << "&data: " << &data << std::endl;
 	std::cout << "serialized: " << serialized << std::endl;
 	std::cout << "deserialized: " << deserialized << std::endl;
-
-	return 0;
 }
