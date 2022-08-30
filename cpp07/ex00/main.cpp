ef80002bfcc184d1b0e7382a9f5b452d68b520b9
seipan1213 <seiju1213@gmail.com>
2022-08-30 18:26:34 +0900

    07 00 実装

---
+++ cpp07/ex00/main.cpp
@@ -0,0 +1,50 @@
+#include "whatever.hpp"
+#include <iostream>
+
+int main()
+{
+	{
+		int a = 10, b = 20;
+
+		std::cout << "INT " << std::endl;
+		std::cout << "max: " << max(a, b) << std::endl;
+		std::cout << "min: " << min(a, b) << std::endl;
+		std::cout << "swap_bf: a: " << a << " b: " << b << std::endl;
+		swap(a, b);
+		std::cout << "swap_af: a: " << a << " b: " << b << std::endl;
+		std::cout << std::endl;
+	}
+	{
+		float a = 5.5f, b = 105.1f;
+
+		std::cout << "Flaot " << std::endl;
+		std::cout << "max: " << max(a, b) << std::endl;
+		std::cout << "min: " << min(a, b) << std::endl;
+		std::cout << "swap_bf: a: " << a << " b: " << b << std::endl;
+		swap(a, b);
+		std::cout << "swap_af: a: " << a << " b: " << b << std::endl;
+		std::cout << std::endl;
+	}
+	{
+		double a = 31.13212, b = 12.3106;
+
+		std::cout << "Double " << std::endl;
+		std::cout << "max: " << max(a, b) << std::endl;
+		std::cout << "min: " << min(a, b) << std::endl;
+		std::cout << "swap_bf: a: " << a << " b: " << b << std::endl;
+		swap(a, b);
+		std::cout << "swap_af: a: " << a << " b: " << b << std::endl;
+		std::cout << std::endl;
+	}
+	{
+		char a = '*', b = '~';
+
+		std::cout << "Char " << std::endl;
+		std::cout << "max: " << max(a, b) << std::endl;
+		std::cout << "min: " << min(a, b) << std::endl;
+		std::cout << "swap_bf: a: " << a << " b: " << b << std::endl;
+		swap(a, b);
+		std::cout << "swap_af: a: " << a << " b: " << b << std::endl;
+		std::cout << std::endl;
+	}
+}
---
+++ cpp07/ex00/whatever.hpp
@@ -0,0 +1,35 @@
+#ifndef WHATERVER_HPP
+#define WHATERVER_HPP
+
+template <typename T>
+T &max(T &a, T &b)
+{
+	return (a >= b ? a : b);
+}
+
+template <typename T>
+const T &max(const T &a, const T &b)
+{
+	return (a >= b ? a : b);
+}
+
+template <typename T>
+T &min(T &a, T &b)
+{
+	return (a <= b ? a : b);
+}
+
+template <typename T>
+const T &min(const T &a, const T &b)
+{
+	return (a <= b ? a : b);
+}
+
+template <typename T>
+void swap(T &a, T &b)
+{
+	T tmp = a;
+	a = b;
+	b = tmp;
+}
+#endif
