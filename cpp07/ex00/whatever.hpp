9846eaec58d81ec90e9338c2363564a4641486f5
seipan1213 <seiju1213@gmail.com>
2022-09-03 00:56:23 +0900

    min,max 等価の場合の処理を修正

--- cpp07/ex00/whatever.hpp
+++ cpp07/ex00/whatever.hpp
@@ -4,25 +4,25 @@
 template <typename T>
 T &max(T &a, T &b)
 {
-	return (a >= b ? a : b);
+	return (a > b ? a : b);
 }
 
 template <typename T>
 const T &max(const T &a, const T &b)
 {
-	return (a >= b ? a : b);
+	return (a > b ? a : b);
 }
 
 template <typename T>
 T &min(T &a, T &b)
 {
-	return (a <= b ? a : b);
+	return (a < b ? a : b);
 }
 
 template <typename T>
 const T &min(const T &a, const T &b)
 {
-	return (a <= b ? a : b);
+	return (a < b ? a : b);
 }
 
 template <typename T>
